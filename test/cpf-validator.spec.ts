f9fe9834869a7843b9912a608dd8299c77b06e70
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-02 21:45:29 -0400

    feat: Add test for returnCpf function in CPF validator module

---
+++ test/cpf-validator.spec.ts
@@ -0,0 +1,8 @@
+import { returnCpf } from "../src/cpf-validator"
+
+describe("Validate Cpf suite", () => {
+    it("Return a valid CPF", () => {
+        const result = returnCpf();
+        expect(result).toBe("123456789-02");
+    });
+})
