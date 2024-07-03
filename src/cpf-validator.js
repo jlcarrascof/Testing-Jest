019ee2c96b4d94da84a950948ff6b46e932644cb
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-02 22:28:06 -0400

    feat: Add add function to CPF validator module

--- src/cpf-validator.js
+++ src/cpf-validator.js
@@ -1,6 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.returnCpf = returnCpf;
+exports.add = add;
 function returnCpf() {
     return "123456789-01";
+}
+function add(a, b) {
+    return a + b;
 }
