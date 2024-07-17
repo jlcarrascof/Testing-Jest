b3f06d8e4fa5a5347dd4fe6cefe228a087f9f7f6
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-17 18:38:18 -0400

    Refactor CPF validator module to improve validation logic and handle CPF length validation

--- src/cpf-validator.js
+++ src/cpf-validator.js
@@ -14,6 +14,31 @@ var CpfValidator = /** @class */ (function () {
         if (cpf.length < 11) {
             return false;
         }
+        // Validate Cpf Digits
+        var sum;
+        var rest;
+        sum = 0;
+        for (var i = 1; i <= 9; i++) {
+            sum = sum + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+        }
+        rest = (sum * 10) % 11;
+        if ((rest == 10) || (rest == 11)) {
+            rest = 0;
+        }
+        if (rest != parseInt(cpf.substring(9, 10))) {
+            return false;
+        }
+        sum = 0;
+        for (var i = 1; i <= 10; i++) {
+            sum = sum + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+        }
+        rest = (sum * 10) % 11;
+        if ((rest == 10) || (rest == 11)) {
+            rest = 0;
+        }
+        if (rest != parseInt(cpf.substring(10, 11))) {
+            return false;
+        }
         return true;
     };
     return CpfValidator;
