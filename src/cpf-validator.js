ef4f0f804ae0bdbc0caf0fbf546446764ed61c3e
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-17 00:48:49 -0400

    feat: Refactor CPF validator module to use CpfValidator class

--- src/cpf-validator.js
+++ src/cpf-validator.js
@@ -1,10 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.returnCpf = returnCpf;
-exports.add = add;
-function returnCpf() {
-    return "123456789-01";
-}
-function add(a, b) {
-    return a + b;
-}
+exports.CpfValidator = void 0;
+var CpfValidator = /** @class */ (function () {
+    function CpfValidator() {
+    }
+    CpfValidator.prototype.validate = function (cpf) {
+        if (!(/^\d+$/.test(cpf))) {
+            return false;
+        }
+    };
+    return CpfValidator;
+}());
+exports.CpfValidator = CpfValidator;
--- test/cpf-validator.spec.ts
+++ test/cpf-validator.spec.ts
@@ -6,15 +6,5 @@ describe("Validate Cpf suite", () => {
         expect(result).toBe("123456789-02");
     });
 
-    it.only("Add two numbers", () => {
-        // arrange
-        const a = 8;
-        const b = 4;
 
-        // act
-        const result = add(a, b);
-
-        // assert
-        expect(result).toBe(12);
-    });
 })
